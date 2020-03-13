4392d709b9ac8c4c94dc899c827cd5328cb9c57f
acolistro <arcolistro@gmail.com>
2020-03-12 21:58:44 -0700

    troubleshoot, update startup file.

--- MyCats/Startup.cs
+++ MyCats/Startup.cs
@@ -25,6 +25,8 @@ namespace Cats
 
     public void Configure(IApplicationBuilder app)
     {
+      app.UseStaticFiles();
+      
       app.UseDeveloperExceptionPage();
 
       app.UseMvc(routes =>
